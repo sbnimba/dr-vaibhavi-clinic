d38c100fa0ab4a2f81e318bf28b1675c4fdcf79a
Saurabh Nimbarte <saurabh@example.com>
2026-05-17 18:31:36 +0530

    Configure GitHub Pages automated deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/dr-vaibhavi-app",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
