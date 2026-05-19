cc61444a844264ccd0ffd42d631e8300a32ed009
Saurabh Nimbarte <saurabh@example.com>
2026-05-19 21:26:37 +0530

    chore: change basePath to /dr-vaibhavi-clinic to support repository name change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/dr-vaibhavi-app",
+  basePath: "/dr-vaibhavi-clinic",
   images: { unoptimized: true },
 };
 
